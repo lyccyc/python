71e04790bdef29d16f7e34298677edb45c7b0ae9
lyccyc <you@example.com>
2023-10-31 20:43:29 +0800

    0720.3.1fin

--- 0720.test.py
+++ 0720.test.py
@@ -1,13 +1,33 @@
-def is_spellable_by_ptable(in_str, p_table: str):
-    for i in in_str:
-        if i in p_table:
+def is_spellable_by_ptable(in_str, p_table):
+    if in_str[0] in p_table:
+        if len(in_str) == 1:
             return True
         else:
-            return False
+            a = list(in_str)
+            u = a.pop(0)
+            in_str = "".join(a)
+            if is_spellable_by_ptable(in_str, p_table) != False:
+                return is_spellable_by_ptable(in_str, p_table)
+            a.insert(0, u)
+            in_str = "".join(a)
+    if in_str[:2] in p_table:
+        if len(in_str) == 2:
+            return True
+        else:
+            a = list(in_str)
+            u = a.pop(0)
+            v = a.pop(0)
+            in_str = "".join(a)
+            if is_spellable_by_ptable(in_str, p_table) != False:
+                return is_spellable_by_ptable(in_str, p_table)
+            a.insert(0, v)
+            a.insert(0, u)
+            in_str = "".join(a)
+    return False
 
 
 if __name__ == "__main__":
     p_table = ["y", "es", "yb", "se", "s", "mg", "ti", "mn", "mt", "as"]
     p_table = [e.lower() for e in p_table]
     print(is_spellable_by_ptable("yes", p_table))
-    print(is_spellable_by_ptable("math", p_table))
+    # print(is_spellable_by_ptable("math", p_table))
